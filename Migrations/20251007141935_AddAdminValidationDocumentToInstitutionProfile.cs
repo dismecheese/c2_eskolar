69da4bc6e5fefb91eaa88d09b7aff6c82635e9c0
raphorenz <150337589+raphorenz@users.noreply.github.com>
2025-10-07 23:30:33 +0800

    Institution Verification, still need UI adjustments

---
+++ Migrations/20251007141935_AddAdminValidationDocumentToInstitutionProfile.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace c2_eskolar.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdminValidationDocumentToInstitutionProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "StudentProfiles");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AdminValidationDocument",
+                table: "InstitutionProfiles",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdminValidationDocument",
+                table: "InstitutionProfiles");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "StudentProfiles",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
