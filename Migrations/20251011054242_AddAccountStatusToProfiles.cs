f30adbb3141741bab2977a5fd3c8b99eb68a0d55
Marzella Capin <capinmarzella2004@gmail.com>
2025-10-11 16:47:57 +0800

    Added archive ui and logic

---
+++ Migrations/20251011054242_AddAccountStatusToProfiles.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace c2_eskolar.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAccountStatusToProfiles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AccountStatus",
+                table: "StudentProfiles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AccountStatus",
+                table: "InstitutionProfiles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AccountStatus",
+                table: "BenefactorProfiles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccountStatus",
+                table: "StudentProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "AccountStatus",
+                table: "InstitutionProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "AccountStatus",
+                table: "BenefactorProfiles");
+        }
+    }
+}
