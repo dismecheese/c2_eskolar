00dc3396888190bfa01f202ce7b459ccb99c820c
raphorenz <150337589+raphorenz@users.noreply.github.com>
2025-10-16 15:45:31 +0800

    .

---
+++ Migrations/20251016074102_AddBenefactorDocumentPaths.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace c2_eskolar.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBenefactorDocumentPaths : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AccreditationDocumentPath",
+                table: "BenefactorProfiles",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BenefactorIdDocumentPath",
+                table: "BenefactorProfiles",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccreditationDocumentPath",
+                table: "BenefactorProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "BenefactorIdDocumentPath",
+                table: "BenefactorProfiles");
+        }
+    }
+}
